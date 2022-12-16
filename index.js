87a1bc071c235fa8af2666b5b2263c2de0014c56
Dmitry Redkin <tanatonaut@gmail.com>
2022-12-16 21:39:33 +0600

    Artifact + mock deploy script

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-exports.tttest = function() {
-    console.log("test");
+exports.Mock = class Mock {
+    ethers
+
+    constructor(ethers) {
+        this.ethers = ethers
+        this.getERC20 = getERC20
+    }
+}
+
+async function getERC20(name, symbol, decimals) {
+    ethers = this.ethers;
+    json = require('./.artifacts/ERC20Mock.json')
+    owner = await ethers.getSigners()
+    ERC20 = await ethers.ContractFactory(json.abi, json.bytecode, owner);
+    erc20 = await ERC20.deploy(name, symbol, decimals)
+    return erc20
 }
