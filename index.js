32fa284a229f8e6add3d3824c8ddfc6c3854f611
Dmitry Redkin <tanatonaut@gmail.com>
2022-12-18 00:42:22 +0600

    finaly

--- index.js
+++ index.js
@@ -35,9 +35,7 @@ async function getUniswapV2Pair(factory, token1, token2) {
     ethers = this.ethers;
     json = require('./.artifacts/UniswapV2Pair.json')
     const [owner] = await ethers.getSigners()
-    console.log("TEST")
-    pairAddress = await factory.getPair(token1.address, token2.address)
-    console.log(pairAddress)
+    pairAddress = await factory.getPair(token1, token2)
     if (pairAddress === "0x0000000000000000000000000000000000000000" ) {
         pairAddress = await factory.callStatic.createPair(token1, token2)
         await factory.createPair(token1, token2)
