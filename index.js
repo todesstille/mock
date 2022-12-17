46b1e7658f4e5ec5bb8dd4d1acf8d9c73ac06766
Dmitry Redkin <tanatonaut@gmail.com>
2022-12-17 19:42:51 +0600

    + weth, uniswap router

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ exports.Mock = class Mock {
         this.ethers = ethers
         this.getERC20 = getERC20
         this.getUniswapV2 = getUniswapV2
+        this.getUniswapV2Factory = getUniswapV2Factory
         this.getUniswapV2Pair = getUniswapV2Pair
+        this.getUniswapV2Router = getUniswapV2Router
+        this.getWeth9 = getWeth9
         this.getChainlinkPricefeed = getChainlinkPricefeed
     }
 }
@@ -19,7 +22,7 @@ async function getERC20(name, symbol, decimals) {
     return erc20
 }
 
-async function getUniswapV2(beneficiary) {
+async function getUniswapV2Factory(beneficiary) {
     ethers = this.ethers;
     json = require('./.artifacts/UniswapV2Factory.json')
     const [owner] = await ethers.getSigners()
@@ -36,6 +39,31 @@ async function getUniswapV2Pair(factory, token1, token2) {
     await factory.createPair(token1, token2)
     pair = await ethers.getContractAt(json.abi, pairAddress, owner);
     return pair
+}
+
+async function getWeth9() {
+    ethers = this.ethers;
+    json = require('./.artifacts/WETH9.json')
+    const [owner] = await ethers.getSigners()
+    Weth9 = await ethers.getContractFactory(json.abi, json.bytecode, owner);
+    weth = await Weth.deploy();
+    return weth
+}
+
+async function getUniswapV2Router(factory, WETH) {
+    ethers = this.ethers;
+    json = require('./.artifacts/UniswapV2Router02.json')
+    const [owner] = await ethers.getSigners()
+    Router = await ethers.getContractFactory(json.abi, json.bytecode, owner);
+    router = await Router.deploy(factory, WETH);
+    return router
+}
+
+async function getUniswapV2(beneficiary) {
+    weth = await this.getWeth9()
+    factory = await this.getUniswapV2Factory(beneficiary)
+    router = await this.getUniswapV2Router(factory.address, weth.address)
+    return [router, factory, weth]
 }
 
 async function getChainlinkPricefeed(decimals, description, version, price) {
