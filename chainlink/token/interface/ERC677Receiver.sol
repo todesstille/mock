5f99701ae31082d68db5cda86fe6ae6110fbd9e3
Dmitry Redkin <tanatonaut@gmail.com>
2022-12-29 01:07:18 +0600

    New comptroller

--- chainlink/token/interface/ERC677Receiver.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
-
-
-interface ERC677Receiver {
-  
-  function onTokenTransfer(address _sender, uint _value, bytes memory _data) external;
-
-}
